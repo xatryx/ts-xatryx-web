0198dea6858b8ea5401806ca57409d56a3281174
ronanharris09 <ronanharris09@gmail.com>
2021-05-01 17:13:21 +0700

    updated index.tsx with grid layout and members list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import Head from 'next/head'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { GetStaticProps } from 'next'
 import Link from 'next/link'
+
+const orgMembers = [
+  {fullName: "Jerens Soehardi Lensun", memberRole: "Roles To be Updated"},
+  {fullName: "Muhammad Hanif Azhary", memberRole: "Roles To be Updated"},
+  {fullName: "Maulina Damayani", memberRole: "Roles To be Updated"},
+  {fullName: "Nadia Indarwati", memberRole: "Roles To be Updated"},
+  {fullName: "Toyib Nurseha", memberRole: "Roles To be Updated"},
+  {fullName: "Ronan Harris", memberRole: "Roles To be Updated"},
+]
 
 export default function Home(): React.ReactElement {
   return (
@@ -19,11 +29,18 @@ export default function Home(): React.ReactElement {
       </header>
       <main className="h-9/10  items-center">
         <h1 className="font-semibold text-3xl py-5 text-center">Our Teams</h1>
-        <div className="grid gap-5 mt-5">
-          <article>
-            <h1>Nama</h1>
-            <p>Software Engineering</p>
-          </article>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-5">
+          {
+            orgMembers.map(item => (
+              <article className="mx-auto" key={item.fullName}>
+                <img src="https://fakeimg.pl/128x128/111111/dddddd/?text=128x128&retina=1" alt="aHuman" className="rounded-full"/>
+                <div className="mt-5 text-center">
+                  <h1>{item.fullName}</h1>
+                  <p>{item.memberRole}</p>
+                </div>
+              </article>
+            ))
+          }
         </div>
       </main>
     </div>
