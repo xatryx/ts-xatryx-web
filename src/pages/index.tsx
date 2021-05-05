0bd395fb04087330c38bfc44df8228f99d9fda01
ronanharris09 <ronanharris09@gmail.com>
2021-05-05 14:03:49 +0700

    updated team-members block title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home(): React.ReactElement {
         </div>
       </header>
       <main className="items-center">
-        <h1 className="font-semibold text-3xl py-5 text-center">Our Teams</h1>
+        <h1 className="font-black text-4xl py-5 text-center">Team Members</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
           {
             teamMembers.map(item => (
